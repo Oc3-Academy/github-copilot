d70974903c3dc654779c276dbbea669b4fe057fc
Luba <luciano.eq@outlook.com>
2023-02-06 04:35:34 -0300

    changes: update code with recording code.

--- 01_image_classification.py
+++ 01_image_classification.py
@@ -1,5 +1,3 @@
-from random import shuffle
-
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -7,85 +5,65 @@ from torchvision import datasets, transforms
 
 # 1. Load data
 train_dataset = datasets.MNIST(
-    root="data",
-    train=True,
-    transform=transforms.ToTensor(),
-    download=True,
+    root="data", train=True, transform=transforms.ToTensor(), download=True
 )
-
 test_dataset = datasets.MNIST(
-    root="data",
-    train=False,
-    transform=transforms.ToTensor(),
-    download=True,
+    root="data", train=False, transform=transforms.ToTensor(), download=True
 )
 
 # 2. Create data loaders
-train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
+train_data_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_data_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-# 3. Create a CNN model
-class CNN(nn.Module):
-    def __init__(self, n_features):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-        )
-        self.fc = nn.Linear(32 * 28 * 28, n_features)
+# 3. Create model
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(28 * 28, 128),
+    nn.ReLU(),
+    nn.Linear(128, 10),
+    nn.Softmax(dim=1),
+).to("cuda")
 
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-
-# 4. Create a loss function and optimizer
-model = CNN(n_features=10)
+# 4. Create loss function
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-# 5. Train the model
-n_epochs = 5
-for epoch in range(n_epochs):
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # 1. Forward pass
-        output = model(data)
-        loss = loss_fn(output, target)
+# 5. Create optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-        # 2. Backward pass
+# 6. Train model
+epochs = 5
+for epoch in range(epochs):
+    for batch_idx, (data, targets) in enumerate(train_data_loader):
+        # 1. Get data to cuda if possible
+        data = data.to("cuda")
+        targets = targets.to("cuda")
+
+        # 2. Forward pass
+        scores = model(data)
+        loss = loss_fn(scores, targets)
+
+        # 3. Backward pass
+        optimizer.zero_grad()
         loss.backward()
 
-        # 3. Update weights
+        # 4. Update parameters
         optimizer.step()
 
-        # 4. Reset gradients
-        optimizer.zero_grad()
-    print(f"Epoch: {epoch + 1}/{n_epochs}, Loss: {loss.item():.4f}")
-
-# 6. Test the model
+# 7. Test the model
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
-    for data, target in test_loader:
-        output = model(data)
-        _, predictions = torch.max(output, 1)
-        n_samples += target.size(0)
-        n_correct += (predictions == target).sum().item()
-    acc = 100.0 * n_correct / n_samples
-    print(f"Accuracy: {acc:.2f}")
+    for data, targets in test_data_loader:
+        data = data.to("cuda")
+        targets = targets.to("cuda")
 
-# 7. Save the model
-torch.save(model.state_dict(), "models/cnn_copilot_experiment.ckpt")
+        scores = model(data)
+        _, predictions = scores.max(1)
+        n_correct += (predictions == targets).sum()
+        n_samples += predictions.size(0)
+
+    acc = 100.0 * n_correct / n_samples
+    print(f"Accuracy: {acc}")
+
+# 8. Save the model
+torch.save(model.state_dict(), "models/model.pth")
--- 03_regex.py
+++ 03_regex.py
@@ -3,15 +3,20 @@ import re
 # regex to match a string that starts with 'a' and ends with 'b'
 regex = re.compile(r"^a.*b$")
 
-regex2 = re.compile(
-    r"""
-    (\d{3}|\(\d{3}\))(:?\s+|-|\.)?(\d{3})(:?\s+|-|\.)?(\d{4})
-    """
-)
+# regex to match a string that starts with 'a' and ends with 'b' and has 'c' in between
+regex = re.compile(r"^a.*c.*b$")
+
+# regex to match numbers in a string
+regex = re.compile(r"\d+")
+
+# regex to match a email address in a string
+regex = re.compile(r"[\w\.-]+@[\w\.-]+")
+
+r"""(\d{3}|\(\d{3}\))(:?\s+|-|\.)?(\d{3})(:?\s+|-|\.)?(\d{4})"""
 # what patterns does this regex match?
-# 123-456-7890
-# 123.456.7890
-# 123 456 7890
-# (123)456-7890
-# (123) 456-7890
+# 1234567890
+# (123)4567890
+# (123) 4567890
 # (123) 456 7890
+# 123 456 7890
+# 123-456-7890
